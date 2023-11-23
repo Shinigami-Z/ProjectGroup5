6008bc9754d5ebfc463001e477cd61e5ff58a13f
ashscripting <arshia.mortazavinezhad@ontariotechu.net>
2023-11-23 17:22:52 -0500

    routes 3.0
    changes to the controller file to handle the new schema.

--- server/routes/InventoryRT.js
+++ server/routes/InventoryRT.js
@@ -1,19 +1,19 @@
 var express = require('express');
 var router = express.Router();
 //const { router } = require('../config/app');
-let Book = require('../models/InventoryRT');
-let AssignmentController = require('../controllers/InventoryRT')
-/* Get route for the Assignments */
+let Products = require('../models/InventoryRT');
+let ProductsController = require('../controllers/InventoryRT')
+/* Get route for the InventoryRT */
 // Read Operation
-router.get('/', AssignmentController.DislayAssignments);
-/* Get route for Add Assignment page --> Create */
-router.get('/add', AssignmentController.AddAssignment);
-/* Post route for Add Assignment page --> Create */
-router.post('/add', AssignmentController.CreateAssignment);
-/* Get route for displaying the Edit Assignment page --> Update */
-router.get('/edit/:id', AssignmentController.EditAssignment);
-/* Post route for processing the Edit Assignment page --> Update */
-router.post('/edit/:id', AssignmentController.ProcessEditAssignment);
+router.get('/', ProductsController.DislayInventoryRT);
+/* Get route for Add Products page --> Create */
+router.get('/add', ProductsController.AddProducts);
+/* Post route for Add Products page --> Create */
+router.post('/add', ProductsController.CreateProducts);
+/* Get route for displaying the Edit Products page --> Update */
+router.get('/edit/:id', ProductsController.EditProducts);
+/* Post route for processing the Edit Products page --> Update */
+router.post('/edit/:id', ProductsController.ProcessEditProducts);
 /* Get to perform Delete Operation --> Delete Operation */
-router.get('/delete/:id', AssignmentController.DeleteAssignment);
+router.get('/delete/:id', ProductsController.DeleteProducts);
  module.exports = router;
